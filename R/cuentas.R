db969f793398720d93deb5bbd167e95ba6b41461
Eloy Eligón <ee.eligon@gmail.com>
2016-02-28 21:08:02 -0430

    Agregar las clasificaciones de siniestros pendientes y gastos administrativos

--- R/cuentas.R
+++ R/cuentas.R
@@ -2,7 +2,8 @@ calcular_area_negocio <- function(codigo_cuenta) {
   codigo_cuenta <- as.character(codigo_cuenta)
   resultado <- rep(NA, length(codigo_cuenta))
   resultado <- ifelse(
-    substr(codigo_cuenta, 1, 2) %in% apply(expand.grid(c(3, 5), c(0, 2, 3, 4)), 1, function(x) paste(x[1], x[2], sep = "")),
+    substr(codigo_cuenta, 1, 2) %in% apply(expand.grid(c(3, 5), c(0, 2, 3, 4)), 1, function(x) paste(x[1], x[2], sep = "")) | codigo_cuenta %in% c(codigos_siniestros_pedientes),
+    # codigo_cuenta %in% c(),
     'Seguros',
     'Gestión general'
   )
@@ -12,11 +13,10 @@ calcular_area_negocio <- function(codigo_cuenta) {
 calcular_area_seguros <- function(codigo_cuenta, area_negocio) {
   codigo_cuenta <- as.character(codigo_cuenta)
   resultado <- rep(NA, length(codigo_cuenta))
-  resultado <- ifelse(area_negocio == 'Seguros' & substr(codigo_cuenta, 2, 2) == '0', 'Personas', resultado)
-  resultado <- ifelse(area_negocio == 'Seguros' & substr(codigo_cuenta, 2, 2) == '2', 'Generales', resultado)
-  resultado <- ifelse(area_negocio == 'Seguros' & substr(codigo_cuenta, 2, 2) == '3', 'Solidarios', resultado)
-  resultado <- ifelse(area_negocio == 'Seguros' & substr(codigo_cuenta, 2, 2) == '4', 'Reaseguros', resultado)
-  # resultado <- factor(resultado, levels = c('Generales', 'Personas', 'Reaseguros', 'Solidarios'))
+  resultado <- ifelse(area_negocio == 'Seguros' & (substr(codigo_cuenta, 2, 2) == '0' | codigo_cuenta %in% codigos_siniestros_pendientes_personas), 'Personas', resultado)
+  resultado <- ifelse(area_negocio == 'Seguros' & (substr(codigo_cuenta, 2, 2) == '2' | codigo_cuenta %in% codigos_siniestros_pendientes_generales), 'Generales', resultado)
+  resultado <- ifelse(area_negocio == 'Seguros' & (substr(codigo_cuenta, 2, 2) == '3' | codigo_cuenta %in% codigos_siniestros_pendientes_solidarios), 'Solidarios', resultado)
+  resultado <- ifelse(area_negocio == 'Seguros' & (substr(codigo_cuenta, 2, 2) == '4' | codigo_cuenta %in% codigos_siniestros_pendientes_reaseguros), 'Reaseguros', resultado)
   resultado
 }
 
@@ -41,7 +41,9 @@ calcular_tipo_monto <- function(codigo_cuenta) {
   resultado <- rep(NA, length(codigo_cuenta))
   resultado <- ifelse(codigo_cuenta %in% codigos_primas, 'Primas', resultado)
   resultado <- ifelse(codigo_cuenta %in% codigos_siniestros_pagados, 'Siniestros Pagados', resultado)
+  resultado <- ifelse(codigo_cuenta %in% codigos_siniestros_pendientes, 'Siniestros Pendientes', resultado)
   resultado <- ifelse(codigo_cuenta %in% codigos_comisiones, 'Comisiones', resultado)
+  resultado <- ifelse(codigo_cuenta %in% codigos_gastos_admin, 'Gastos Administrativos', resultado)
   resultado
 }
 
