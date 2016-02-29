6abd71ae97eb2459b5ebf39ce4f27e9448e38772
Eloy Eligón <ee.eligon@gmail.com>
2016-02-28 21:08:27 -0430

    Nuevos códigos

--- data-raw/codigos.R
+++ data-raw/codigos.R
@@ -3,13 +3,38 @@ codigos_primas_devueltas <- c('30103', '32102', '33102', '34102')
 codigos_primas <- c(codigos_primas_cobradas ,codigos_primas_devueltas)
 
 codigos_primas_personas <- c('50101', '30103')
-codigos_primas_solidarios <- c('52101', '32102')
-codigos_primas_generales <- c('53101', '33102')
+codigos_primas_generales <- c('52101', '32102')
+codigos_primas_solidarios <- c('53101', '33102')
 codigos_primas_reaseguros <- c('54101', '54102', '34102')
 
 codigos_siniestros_pagados <- c('30101', '30102', '32101', '33101', '34101')
 
+codigos_siniestros_pendientes_personas <- c("4010401", "4010402", "4010403", "4010404")
+codigos_siniestros_pendientes_solidarios <- c("4010406")
+codigos_siniestros_pendientes_generales <- c("4010405")
+codigos_siniestros_pendientes_reaseguros <- c("40105")
+
+codigos_siniestros_pendientes <- c(
+  codigos_siniestros_pendientes_personas,
+  codigos_siniestros_pendientes_solidarios,
+  codigos_siniestros_pendientes_generales,
+  codigos_siniestros_pendientes_reaseguros
+)
+
 codigos_comisiones <- c('30104', '32103', '33103', '34103')
+codigos_gastos_admin_personas <- '30109'
+codigos_gastos_admin_generales <- '32109'
+codigos_gastos_admin_solidarios <- '33109'
+codigos_gastos_admin_reaseguros <- '34109'
+codigos_gastos_admin_gestion <- '38109'
+
+codigos_gastos_admin <- c(
+  codigos_gastos_admin_personas,
+  codigos_gastos_admin_solidarios,
+  codigos_gastos_admin_generales,
+  codigos_gastos_admin_reaseguros,
+  codigos_gastos_admin_gestion
+)
 
 use_data(
   codigos_primas_cobradas,
@@ -20,7 +45,9 @@ use_data(
   codigos_primas_generales,
   codigos_primas_reaseguros,
   codigos_siniestros_pagados,
+  codigos_siniestros_pendientes,
   codigos_comisiones,
+  codigos_gastos_admin,
   internal = TRUE,
   overwrite = TRUE
 )
